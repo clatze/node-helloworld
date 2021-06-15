37de80d901881fc70a7e5b22d30cc58523d59cda
clatze <latze@angry-red-pla.net>
2021-06-15 17:13:36 +0200

    should work

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 // Run `node index.js` in the terminal
-const http = require('http');
 
 console.log(`Hello Node.js v${process.versions.node}!`);
 
 // testing http client
-http
-  .get('http://example.org', response => {
-    console.log('inside get');
-    let todo = '';
-    response.on('data', chunk => {
-      todo += chunk;
-      console.log('reading data');
-    });
-
-    response.on('end', () => {
-      console.log('read data to end');
-      console.log(todo);
-    });
-  })
-  .on('error', error => {
-    console.log('Error: ' + error.message);
-  });
+var request = require('request');
+request('http://www.google.com', function(error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
 
 console.log('END');
